bbb508f9f2fc2b010c2d0c71d20e9bf66c407c55
KiHwang-Park <[parkkh702@naver.com]>
2023-11-11 11:07:20 +0900

    배열에서 특정한 값 찾기

---
+++ Searchint.py
@@ -0,0 +1,8 @@
+def solution(num_list, n):
+    if num_list.count(n)==0:  # (리스트).count(값) = 0 이면 해당 값은 리스트에 없다
+        return 0              # 해당 값이 없는 상황이므로 0 을 반환
+    else:
+        return 1
+
+
+
